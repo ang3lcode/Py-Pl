fac761d1c7263177fd1e6f71b1917077a6b228dc
angel melendez <angelmelen88@gmail.com>
2024-10-15 11:10:20 -0600

    feat:file condicion

---
+++ 01py-curso/04_Estructura_condicionales.py
@@ -0,0 +1,27 @@
+is_member = True
+age = 11
+x = 10
+y =20
+
+if is_member:
+    if age>=15:
+      print("mayor a 15")
+    else :
+      print("menor a 15")
+else:
+    print("no es false")
+
+if x > 5:
+    print("x es mayot a 5")
+elif x == 5:
+    print("X es igual que 5")
+else: 
+    print("X es menor que 5")
+print("afuera")
+
+if x > 10 and y > 25:
+    print("X es mayor que 10 y Y es mayor que 15")
+if x > 10 or y > 25:
+    print("X es mayor que 10 O Y es Mayor que 25")
+if not x > 10:
+    print("X no es mayor que 10")
